ada56ff8f3ab9a1c1fb6b30c5a5583c5d977502d
Sharad <Sharad@DESKTOP-BKC8SOF>
2026-04-25 17:15:47 +0530

    project setup

--- backend/Data/IAppDbContext.cs
+++ backend/Data/IAppDbContext.cs
@@ -1,35 +1,8 @@
-using Microsoft.EntityFrameworkCore;
-using RB_Website_API.DTO;
-using RB_Website_API.Models;
-using RB_Website_API.Models.Lead;
-using RB_Website_API.ViewModels;
-
 namespace RB_Website_API.Data
 {
     public interface IAppDbContext
     {
-        public DbSet<UserOtp> UserOtp { get; set; }
-        public DbSet<OtpRequestLog> OtpRequestLog { get; set; }
-        public DbSet<ProfileViewModel> ProfileViewModel { get; set; }
-        public DbSet<DashboardViewModel> DashboardViewModel { get; set; }
-        public DbSet<CompanyStats> CompanyStats { get; set; }
-        public DbSet<LoanRates> LoanRates { get; set; }
-        public DbSet<lenderMaster> lenderMaster { get; set; }
-        public DbSet<LoanProducts> LoanProducts { get; set; }
-        public DbSet<LenderProductRates> LenderProductRates { get; set; }
-        public DbSet<RB_ContactRequests> RB_ContactRequests { get; set; }
-        public DbSet<RB_OfficeLocations> RB_OfficeLocations { get; set; }
-        public DbSet<Pincode_Master_ID> Pincode_Master_ID { get; set; }
-        public DbSet<RB_JobDto> RB_JobDto { get; set; }
-        public DbSet<RB_JobApplications> RB_JobApplications { get; set; }
-        public DbSet<RB_Vacancy_Master> RB_Vacancy_Master { get; set; }
-        public DbSet<RB_Vacancy_Details> RB_Vacancy_Details { get; set; }
-        public DbSet<RB_Events> RB_Events { get; set; }
-        public DbSet<RB_TestimonialMedia> RB_TestimonialMedia { get; set; }
-        public DbSet<Lead_data> lead_Data { get; set; }
-        public DbSet<CreditRepairLead> CreditRepairLeads { get; set; }
-        public DbSet<Designation_Master> Designation_Master { get; set; }
-
+       
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
         int SaveChanges();
